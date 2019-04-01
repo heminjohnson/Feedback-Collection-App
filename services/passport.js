14ae68cb841e938cd34ba56b1328d431b8e16ab4
Hemin Johnson <achayan1992@gmail.com>
2019-04-01 23:21:32 +0200

    feat: use passport auth flow to decode users and pass data to a cookie
    This happens if passport could find the user in the database

--- services/passport.js
+++ services/passport.js
@@ -10,6 +10,12 @@ passport.serializeUser((user, done) => {
 	done(null, user)
 })
 
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+})
+
 passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientId,

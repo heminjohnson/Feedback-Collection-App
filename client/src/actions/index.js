21d3f2c21a85fe433ea7e72cb174b9918c2981dd
Hemin Johnson <achayan1992@gmail.com>
2019-04-07 16:26:48 +0200

    feat: connect actions reducers and the app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,5 +3,5 @@ import { FETCH_USER } from './types'
 
 export const fetchUser = () => async dispatch => {
   const res = axios.get('/api/current_user')
-  dispatch({type: FETCH_USER, payload: res})
+  dispatch({type: FETCH_USER, payload: res.data})
 }

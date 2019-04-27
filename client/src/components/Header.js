44f14ae46321f66b3bd60686afe3faecdeb2b9df
Hemin Johnson <achayan1992@gmail.com>
2019-04-27 18:51:55 +0200

    feat: update the header to use the prop data if the user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href='/auth/google'>Login with Google</a></li>
+      default:
+        return <li><a href='/api/logout'>Logout</a></li>
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className='nav-wrapper'>
           <div className='container'>
-            <a className='left brand-logo'>
+            <Link to={ this.props.auth ? '/surveys' : '/' } className='left brand-logo'>
               Feedback-Collection-App
-            </a>
+            </Link>
             <ul className='right'>
-              <li>
-                <a>Login with Google</a>
-              </li>
+              {this.renderContent()}
             </ul>
           </div>
         </div>
@@ -21,4 +32,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps ({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)

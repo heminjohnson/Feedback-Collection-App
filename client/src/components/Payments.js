908cd821fbbded3796ae94c411ceb8bbf15fd43b
Hemin Johnson <achayan1992@gmail.com>
2019-04-30 21:33:02 +0200

    feat: update stripe checkout to use styled buttons

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -9,7 +9,11 @@ class Payments extends Component {
         description={'$5 for 5 email credits'}
         amount={500}
         token={token => console.log(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}/>
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}>
+          <button className={'btn'}>
+            Add Credits
+          </button>
+      </StripeCheckout>
     )
   }
 }

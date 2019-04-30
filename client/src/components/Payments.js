1d7c234dbae6f13dbc99c2813afdf6830b64608f
Hemin Johnson <achayan1992@gmail.com>
2019-04-30 21:30:35 +0200

    fix(stripeCheckout): add name and description prop

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,7 +4,12 @@ import StripeCheckout from 'react-stripe-checkout'
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout  amount={500} token={token => console.log(token)} stripeKey={process.env.REACT_APP_STRIPE_KEY}/>
+      <StripeCheckout
+        name={'Feedback-Collection-App'}
+        description={'$5 for 5 email credits'}
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}/>
     )
   }
 }

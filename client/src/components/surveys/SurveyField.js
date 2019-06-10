cf5aa34e7b9912342bf6e41e3d5d28cc565c0f6e
Hemin Johnson <achayan1992@gmail.com>
2019-06-10 23:57:43 +0200

    feat: add a custom field input component

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,14 @@
+// SurveyField contains logic to render a single label and text input
+import React, { Component } from 'react'
+
+class SurveyField extends Component {
+  render () {
+    return (
+      <div>
+        <input/>
+      </div>
+    )
+  }
+}
+
+export default SurveyField

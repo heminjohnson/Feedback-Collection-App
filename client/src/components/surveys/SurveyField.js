f99d547ce61bd5a77da855f80ca93e5190e84dff
Hemin Johnson <achayan1992@gmail.com>
2019-06-15 22:07:52 +0200

    feat: add all the fields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 
 class SurveyField extends Component {
   render () {
-    const { input } = this.props
+    const { input, label } = this.props
     return (
       <div>
+        <label>{label}</label>
         <input {...input}/>
       </div>
     )

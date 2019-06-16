5661bf4e259d2e60beaf96cec974d5f7b0efb8c2
Hemin Johnson <achayan1992@gmail.com>
2019-06-16 17:45:49 +0200

    feat: generalize field validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -7,8 +7,10 @@ class SurveyField extends Component {
     return (
       <div>
         <label>{label}</label>
-        <input {...input}/>
-        { touched && error }
+        <input {...input} style={{ marginBottom: '5px' }}/>
+        <div className={'red-text'} style={{ marginBottom: '20px' }}>
+          { touched && error }
+        </div>
       </div>
     )
   }

0a75b69b86f2e291183e2fa57e74fa2673f37262
Hemin Johnson <achayan1992@gmail.com>
2019-07-15 21:15:55 +0200

    feat: add survey list component

--- client/src/components/surveys/surveyList.js
+++ client/src/components/surveys/surveyList.js
@@ -8,10 +8,28 @@ class SurveyList extends Component {
     this.props.fetchSurveys()
   }
 
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className={'card darken-1'} key={survey._id}>
+          <div className={'card-content'}>
+            <span className={'card-title'}>{ survey.title }</span>
+            <p>{ survey.body }</p>
+            <p className={'right'}> sent on: { new Date(survey.lastResponded).toLocaleDateString() }</p>
+          </div>
+          <div className={'card-action'}>
+            Yes: {survey.yes}&nbsp;
+            No: {survey.no}
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        SurveyList
+        { this.renderSurveys() }
       </div>
     )
   }
